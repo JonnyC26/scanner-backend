f0d9c2e81114b0e54821048d9e60276435d3b363
JonnyC26 <jonnycan26@gmail.com>
2026-07-01 12:03:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
 
 function calculateScore(nutriScore, novaGroup, additivesCount, isOrganic, protein, sugar, sodium) {
